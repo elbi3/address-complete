3134d1a1316a6601b907aaf427f24eb3ccf2e278
elbi3 <ellbi3@fastmail.com>
2024-08-23 12:17:55 -0700

    Initial commit and html boilerplate

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+
+</script>
+
+<div class="body">
+    <header>
+        <h1>Get Address</h1>
+    </header>
+    <main>
+        <form class="my-form" method="POST">
+            <div class="input-cont">
+                <label for="street">Street Address: </label>
+                <input type="text" name="street">
+            </div>
+            <div class="input-cont">
+                <label for="locality">City</label>
+                <input type="text" name="locality">
+            </div>
+            <div class="input-cont">
+                <label for="region">Country</label>
+                <input type="text" name="region">
+            </div>
+            <div class="input-submit">
+                <button type="submit">Get</button>
+            </div>
+        </form>
+    </main>
+    <footer>
+
+    </footer>
+</div>
