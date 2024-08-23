bbb76d4f20183b7a57175edd00ffbae9fef8a1e8
elbi3 <ellbi3@fastmail.com>
2024-08-23 15:13:04 -0700

    Add address authentication to form action

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script>
-
+    import { enhance } from '$app/forms';
+    // export let data;
+    export let form;
+    console.log("form ? ", form);
 </script>
 
 <div class="body">
@@ -7,23 +10,37 @@
         <h1>Get Address</h1>
     </header>
     <main>
-        <form class="my-form" method="POST">
+        {#if form?.error}
+        <p>{form.error}</p>
+        {/if}
+        <form class="my-form" method="POST" use:enhance>
             <div class="input-cont">
-                <label for="street">Street Address: </label>
-                <input type="text" name="street">
+                <label for="streetAddress">Street Address: </label>
+                <input type="text" name="street" value={form?.street ?? ''} id="streetAddress">
             </div>
             <div class="input-cont">
-                <label for="locality">City</label>
-                <input type="text" name="locality">
+                <label for="myLocality">City</label>
+                <input type="text" name="locality" value={form?.locality ?? ''} id="myLocality">
             </div>
             <div class="input-cont">
-                <label for="region">Country</label>
-                <input type="text" name="region">
+                <label for="myRegion">Country</label>
+                <input type="text" name="region" value={form?.region ?? ''} id="myRegion">
             </div>
             <div class="input-submit">
                 <button type="submit">Get</button>
             </div>
         </form>
+        <div>
+            <h2>data returned from the request</h2>
+            <ul>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+            </ul>
+        </div>
+        {#if form?.success}
+            <p>Successfully submitted!</p>
+        {/if}
     </main>
     <footer>
 
